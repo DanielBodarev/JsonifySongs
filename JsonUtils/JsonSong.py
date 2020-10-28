780e82f57d70f94c28d5f904cd114013e1bc19fc
Daniel Bodarev <daniel.bodarev@jessup.edu>
2020-10-28 11:01:49 -0700

    Added TODO with 3 priorities. Some keys auto-added

--- JsonUtils/JsonSong.py
+++ JsonUtils/JsonSong.py
@@ -3,14 +3,10 @@ class JsonSong:
     def __init__(self, textblocks, title):
         self.textblocks = self._textblock_as_json_format(textblocks)
         self.title = title
-        self.guid = "60g30Z4xsESNEmW8Las0eg"
-        self.video_duration = 0
 
     def get_as_json_format(self):
         result = {
-            "Guid" : self.guid,
             "Verses" : self.textblocks,
-            "VideoDuration" : self.video_duration,
             "Text" : self.title
         }
 
