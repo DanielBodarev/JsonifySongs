3387d00d9b7fe7b4a02ab707752f9eb60133d094
Daniel Bodarev <daniel.bodarev@jessup.edu>
2020-10-28 20:33:39 -0700

    Settings now has source songs folder

--- toVideoPsalm.py
+++ toVideoPsalm.py
@@ -1,7 +1,9 @@
-from SongIO import FromFolder
-from SongIO import SaveSongbook
+from Common import Settings
 from JsonUtils.JsonSongbook import JsonSongbook
+from SongIO import FromFolder, SaveSongbook
 
-song_arrays = get_song_array_from_folder("path")
+source_folder = Settings.misc.get("sourcefolder")
+
+song_arrays = get_song_array_from_folder(source_folder)
 songbook = JsonSongbook(song_arrays)
 save_songbook(songbook)
